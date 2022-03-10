0e3c8d6347c6378a0c43bef6b949da615f4d80db
Isabelle Russell <isabelle.russell@outlook.com>
2022-03-10 11:22:13 +0100

    more progress

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    const principal = parseFloat(document.getElementById("principal").value);
+    const rate = parseFloat(document.getElementById("rate").value);
+    const years = parseFloat(document.getElementById("years").value);
+    const amount = principal * years * rate / 100;
+    const year = new Date().getFullYear() + parseInt(years);
+    const html = "If you deposit " + principal +
+        ",\<br\>at an interest rate of " + rate +
+        "%\<br\>You will receive an amount of " + amount +
+        ",\<br\>in the year " + year + "\<br\>"
+    document.getElementById("result").innerHTML = html
 }
-        
+
+function rate() {
+
+
+    return rate
+}
+
+function updateRate() {
+    const rateval = parseFloat(document.getElementById("rate").value);
+    document.getElementById("rate_val").innerText = `${rateval.toFixed(1)}%`;
+}
