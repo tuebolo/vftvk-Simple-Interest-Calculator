384470eda74a54bee0539d69f98fa65bdb6b6f4f
Isabelle Russell <isabelle.russell@outlook.com>
2022-03-13 12:17:19 +0100

    Finish exercise

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 function compute() {
-    const principal = parseFloat(document.getElementById("principal").value);
+    document.getElementById("result").innerHTML = ""
+
+    const principalElement = document.getElementById("principal");
+    const principal = parseFloat(principalElement.value);
+    if (principal <= 0) {
+        alert( "Enter a positive number")
+        principalElement.focus()
+        return
+    }
+
+
     const rate = parseFloat(document.getElementById("rate").value);
     const years = parseFloat(document.getElementById("years").value);
-    const amount = principal * years * rate / 100;
+    const amount = principal * (rate / 100 * years);
     const year = new Date().getFullYear() + parseInt(years);
-    const html = "If you deposit " + principal +
-        ",\<br\>at an interest rate of " + rate +
-        "%\<br\>You will receive an amount of " + amount +
-        ",\<br\>in the year " + year + "\<br\>"
+    const html = `If you deposit <span class="highlight">${principal}</span>
+        <br\>at an interest rate of <span class="highlight">${rate}</span>
+        <br\>You will receive an amount of <span class="highlight">${amount.toFixed(2)}</span>
+        <br\>in the year <span class="highlight">${year}</span> <br\>`
     document.getElementById("result").innerHTML = html
-}
-
-function rate() {
-
-
-    return rate
 }
 
 function updateRate() {
     const rateval = parseFloat(document.getElementById("rate").value);
-    document.getElementById("rate_val").innerText = `${rateval.toFixed(1)}%`;
+    document.getElementById("rate_val").innerText = `${rateval.toFixed(2)}%`;
 }
+
+window.addEventListener('load', function() {
+    updateRate()
+})
